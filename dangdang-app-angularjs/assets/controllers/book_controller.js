f317ae7ff557334412f3eb5d8fccc69058a008e2
dzwing <mac@Mac-mini.local>
2016-10-19 17:11:11 +0800

    修改图片路径

--- dangdang-app-angularjs/assets/controllers/book_controller.js
+++ dangdang-app-angularjs/assets/controllers/book_controller.js
@@ -1,8 +1,8 @@
 app.controller('bookController',['$scope',function($scope){
   $scope.bookTypes=[
-    {id:'ertong',name:'儿童',img:'../images/book_ertong.jpg',description:''},
-    {id:'lishi',name:'历史',img:'../images/book_lishi.jpg',description:''},
-    {id:'dongman',name:'动漫',img:'../images/book_dongman.jpg',description:''},
-    {id:'qingchunwenxue',name:'青春文学',img:'../images/book_qingchunwenxu.jpg',description:''}
+    {id:'ertong',name:'儿童',img:'./images/book_ertong.jpg',description:''},
+    {id:'lishi',name:'历史',img:'./images/book_lishi.jpg',description:''},
+    {id:'dongman',name:'动漫',img:'./images/book_dongman.jpg',description:''},
+    {id:'qingchunwenxue',name:'青春文学',img:'./images/book_qingchunwenxu.jpg',description:''}
   ]
 }])
