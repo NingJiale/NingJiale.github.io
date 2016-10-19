97ba3291cac1ca30f9b716b8d5217fc588157299
dzwing <mac@Mac-mini.local>
2016-10-19 22:03:23 +0800

    数据展示

--- dangdang-app-angularjs/assets/controllers/book_list_controller.js
+++ dangdang-app-angularjs/assets/controllers/book_list_controller.js
@@ -1,5 +1,6 @@
 app.controller('bookListController', ['$scope', 'commonService', '$routeParams', function($scope,c_service,$routeParams) {
     c_service.getData($routeParams.id,function(res){
-      console.dir(res);
+      // console.dir(res.data);
+     $scope.bookData = res.data;
     })
 }])
